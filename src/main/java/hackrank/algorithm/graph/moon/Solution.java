098bb5800f89c45b10ac2a0b5a0eb7a338cb8363
Vlad Poskatcheev <vposkatcheev@gmail.com>
2015-12-24 00:58:14 -0800

    Journey to the Moon - finished and working

--- src/main/java/hackrank/algorithm/graph/moon/Solution.java
+++ src/main/java/hackrank/algorithm/graph/moon/Solution.java
@@ -3,163 +3,129 @@ package hackrank.algorithm.graph.moon;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * Journey to the Moon
  * 
  * @see https://www.hackerrank.com/challenges/journey-to-the-moon
+ * @see http://algs4.cs.princeton.edu/15uf/ (nice chapter about union find)
  */
 public class Solution {
 
 	public static void main( String[] args ) {
-		Graph graph = readInput( System.in );
-		// System.out.println( graph.countForests() );
-		System.out.println( graph.countPermutations() );
-	}
-
-	private static Graph readInput( InputStream input ) {
-		Scanner scanner = new Scanner( input );
-
-		int numberNodes = scanner.nextInt();
-		int numberEdges = scanner.nextInt();
-
-		int[][] edges = new int[ numberEdges ][];
-		for ( int e = 0; e < numberEdges; e++ ) {
-			edges[ e ] = new int[] { scanner.nextInt(), scanner.nextInt() };
-		}
-
-		scanner.close();
-
-		return new Graph( numberNodes, edges );
-	}
-}
-
-class Graph {
-
-	public Map<Integer, Node> nodes;
-
-	public Graph( int numberNodes, int[][] edges ) {
-		this.nodes = new HashMap<>( numberNodes, 1.0f );
-
-		for ( int nodeId = 0; nodeId < numberNodes; nodeId++ ) {
-			nodes.put( nodeId, new Node( nodeId ) );
-		}
-
-		for ( int[] edge : edges ) {
-			connect( edge[ 0 ], edge[ 1 ] );
-		}
-	}
-
-	public void connect( int idFrom, int idTo ) {
-		Node from = nodes.get( idFrom );
-		Node to = nodes.get( idTo );
-		from.connect( to );
-		to.connect( from );
+		UnionFind unionFind = readInput( System.in );
+		System.out.println( countPermutations( unionFind.getSetSizes() ) );
 	}
 	
-	public List<Integer> countForests() {
+	public static long countPermutations( List<Integer> setSizes ) {
 		
-		List<Integer> counts = new ArrayList<>();
-		Set<Integer> visited = new HashSet<>();
+		List<Integer> optimizedSizes = new ArrayList<>();
 		
-		for ( int nodeId : nodes.keySet() ) {
-			
-			if ( visited.contains( nodeId ) ) {
-				continue;
+		// Optimization: count number of sets of size 1 to simplify later multiplication
+		long singleNodes = 0;
+		for ( int size : setSizes ) {
+			if( size == 1 ) {
+				singleNodes++;
+			} else {
+				optimizedSizes.add( size );
 			}
-			
-			Node node = nodes.get( nodeId );
-			visited.add( node.id );
-			
-			int count = 1;
-			for ( Node descendant : node.getDescendants() ) {
-				visited.add( descendant.id );
-				count++;
-			}
-			
-			counts.add( count );
 		}
 		
-		return counts;
-	}
-	
-	public int countPermutations() {
-		List<Integer> counts = countForests();
+		if ( singleNodes > 0 ) {
+			optimizedSizes.add( (int)singleNodes );	
+		}
 		
-		int permutations = 0;
-		for ( int i = 0; i < counts.size() - 1; i++ ) {
-			for ( int j = i + 1; j < counts.size(); j++ ) {
-				permutations += counts.get( i ) * counts.get( j );
+		long permutations = 0;
+		
+		for ( int i = 0; i < optimizedSizes.size() - 1; i++ ) {
+			for ( int j = i + 1; j < optimizedSizes.size(); j++ ) {
+				permutations += optimizedSizes.get( i ) * optimizedSizes.get( j );
 			}
+		}
+		
+		if ( singleNodes > 0 ) {
+			permutations += singleNodes * (singleNodes - 1) / 2;	
 		}
 		
 		return permutations;
 	}
-	
+
+	private static UnionFind readInput( InputStream input ) {
+		Scanner scanner = new Scanner( input );
+
+		int numberNodes = scanner.nextInt();
+		int numberEdges = scanner.nextInt();
+		
+		UnionFind unionFind = new UnionFind( numberNodes );
+		
+		for ( int e = 0; e < numberEdges; e++ ) {
+			unionFind.union( scanner.nextInt(), scanner.nextInt() );
+		}
+
+		scanner.close();
+
+		return unionFind;
+	}
 }
 
-class Node {
+class UnionFind {
+	
+	private int[] nodeLeads;
 
-	public int id;
-	public List<Edge> edges;
-
-	public Node( int id ) {
-		this.id = id;
-		this.edges = new ArrayList<>();
-	}
-
-	public void connect( Node square ) {
-		edges.add( new Edge( square ) );
+	public UnionFind( int numberNodes ) {
+		nodeLeads = new int[ numberNodes ];
+		for ( int i = 0; i < numberNodes; i++ ) {
+			nodeLeads[ i ] = i;
+		}
 	}
 	
-	public List<Node> getDescendants() {
-		return getDescendants( null );
+	public int getNumberNodes() {
+		return nodeLeads.length;
 	}
 	
-	private List<Node> getDescendants( Node parent ) {
-		List<Node> descendants = new ArrayList<>();
+	public List<Integer> getSetSizes() {
 		
-		for ( Edge edge : edges ) {
-			if ( parent != null && parent.id == edge.target.id ) {
-				continue;
-			}
+		Map<Integer,Integer> sizes = new HashMap<>();
+		
+		for ( int nodeId = 0; nodeId < nodeLeads.length; nodeId++ ) {
+			int nodeLeadId = find( nodeId );
 			
-			descendants.add( edge.target );
-			
-			for ( Node descendant : edge.target.getDescendants( this ) ) {
-				if ( descendant.id != edge.target.id ) {
-					descendants.add( descendant );
-				}
+			if ( ! sizes.containsKey( nodeLeadId ) ) {
+				sizes.put( nodeLeadId, 1 );
+			} else {
+				int size = sizes.get( nodeLeadId );
+				sizes.put( nodeLeadId, size + 1 );
 			}
 		}
 		
+		return new ArrayList<>( sizes.values() );
+	}
+
+	public int find( int nodeId ) {
 		
-		return descendants;
+		int nodeLeadId = nodeId;
+		while ( nodeLeadId != nodeLeads[ nodeLeadId ] ) {
+			nodeLeadId = nodeLeads[ nodeLeadId ];
+		}
+		
+		while ( nodeId != nodeLeadId ) {
+			int nextNodeId = nodeLeads[ nodeId ];
+			nodeLeads[ nodeId ] = nodeLeadId; // compress path: re-point to new lead id
+			nodeId = nextNodeId;
+		}
+		
+		return nodeLeadId;
 	}
-
-	@Override
-	public String toString() {
-		return String.valueOf( id );
-	}
-
-}
-
-class Edge {
-
-	public Node target;
-
-	public Edge( Node target ) {
-		this.target = target;
-	}
-
-	@Override
-	public String toString() {
-		return "-> N(" + target.id + ")";
+	
+	public void union( int firstId, int secondId ) {
+		int firstLeadId = find( firstId );
+		int secondLeadId = find( secondId );
+		if ( firstLeadId != secondLeadId ) {
+			nodeLeads[ firstLeadId ] = secondLeadId;	
+		}
 	}
 
 }
