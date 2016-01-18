b4b57f106c265a292c4591e907dac54238ef230c
Vlad Poskatcheev <vposkatcheev@gmail.com>
2016-01-17 23:18:14 -0800

    Coin Change Problem - work in progress

--- src/main/java/hackrank/algorithm/graph/coin/Solution.java
+++ src/main/java/hackrank/algorithm/graph/coin/Solution.java
@@ -2,9 +2,9 @@ package hackrank.algorithm.graph.coin;
 
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+
+// http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
 
 /**
  * Coin Change Challenge
@@ -14,148 +14,73 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main( String[] args ) {
-		CoinChanger changer = readInput( System.in );
-		System.out.println( changer.countWaysMakeChange() );
+		 CoinChanger changer = readInput( System.in );
+		 System.out.println( changer.countWaysMakeChange() );
 	}
-	
+
+	public static long countNoMem( int coins[], int available, int value ) {
+		if ( value == 0 ) {
+			return 1;
+		}
+
+		if ( value < 0 ) {
+			return 0;
+		}
+
+		if ( available <= 0 && value >= 1 ) {
+			return 0;
+		}
+
+		int reducedValue = value - coins[ available - 1 ];
+		return countNoMem( coins, available - 1, value ) + countNoMem( coins, available, reducedValue );
+	}
+
 	public static CoinChanger readInput( InputStream stream ) {
-	
+
 		Scanner scanner = new Scanner( stream );
-		
+
 		int amount = scanner.nextInt();
 		int size = scanner.nextInt();
-		
+
 		int[] coins = new int[ size ];
 		for ( int i = 0; i < size; i++ ) {
 			coins[ i ] = scanner.nextInt();
 		}
-		
+
 		scanner.close();
-		
+
 		return new CoinChanger( amount, coins );
 	}
 
 }
 
 class CoinChanger {
-	
+
 	private int amount;
 	private int[] coins;
-	private int count;
-	private Map<Integer,Integer> results;
-	
+
 	CoinChanger( int amount, int[] coins ) {
 		this.amount = amount;
 		this.coins = coins;
-		this.results = new HashMap<>();
 	}
-	
-	public int countWaysMakeChange() {
-		
+
+	public long countWaysMakeChange() {
 		Arrays.sort( coins );
-		count = 0;
-		countWaysMakeChange( 0, 0 );
-		return count;
+		return count( coins.length, amount );
 	}
 	
-	private void countWaysMakeChange( int startIndex, int total ) {
+	private long count( int available, int value ) {
 		
-		for ( int index = startIndex; index < coins.length; index++ ) {
-			int coin = coins[ index ];
-			
-			int newTotal = coin + total;
-			if ( newTotal > amount ) {
-				break; // no point in trying any further coins since they are greater
-			} else if ( newTotal == amount ) {
-				count++;
-				break;
-			} else {
-				countWaysMakeChange( index, newTotal );	
+		long[] table = new long[ value + 1 ];
+		
+		table[ 0 ] = 1;
+
+		for ( int i = 0; i < available; i++ ) {
+			for ( int j = coins[ i ]; j <= value; j++ ) {
+				table[ j ] += table[ j - coins[ i ] ];
 			}
 		}
+
+		return table[ value ];
 	}
-	
-	public int countWaysMakeChange2() {
-		
-		Arrays.sort( coins );
-		
-		this.results.clear();
-		
-		int smallestCoin = coins[ 0 ];
-		int maxNumberCoins = amount / smallestCoin; 
-		
-		count = 0;
-		countWaysMakeChange2( 0, amount, maxNumberCoins );
-		return count;
-	}
-	
-	public void countWaysMakeChange2( int startIndex, int value, int availableCoins ) {		
-		if ( availableCoins == 0 ) {
-			return;
-		}
-		
-		if ( hasCalculation( value ) ) {
-			count += getCalculation( value );
-			return;
-		}
-		
-		for ( int index = startIndex; index < coins.length; index++ ) {
-			int coin = coins[ index ];
-			
-			int reducedValue = value - coin;
-			
-			if ( hasCalculation( reducedValue ) ) {
-				int prevCount = getCalculation( reducedValue );
-				saveCalculation( value, prevCount > 0 );
-				count += prevCount;
-				break;
-			}
-			
-			if ( reducedValue == 0 ) {
-				saveCalculation( value, true );
-				count++;
-				break;
-			} else if ( reducedValue < 0 ) {
-				saveCalculation( value, false );
-				break;
-			} else {
-				countWaysMakeChange2( index, reducedValue, availableCoins - 1 );
-			}
-		}
-		
-		// subtracting the same numbers over and over again + recursive calls ... how can you remember that?
-		
-		/*
-		 * 1-3-5
-		 * 
-		 * Example: $4 remaining
-		 * 
-		 * 4 -> List<String> 1-1-1-1, 1-3
-		 * 
-		 * remaining -> count
-		 * 			 * 
-		 * value 4, need to know how many ways I can getd to 0 from there
-		 */
-		
-	}
-	
-	private void saveCalculation( int value, boolean success ) {
-		if ( ! results.containsKey( value ) ) {
-			results.put( value, 0 );
-		}
-		
-		if ( success ) {
-			int count = results.get( value );
-			results.put( value, count + 1 );
-		}
-	}
-	
-	private boolean hasCalculation( int value ) {
-		return results.containsKey( value );
-	}
-	
-	private int getCalculation( int value ) {
-		return results.get( value );
-	}
-	
 }
