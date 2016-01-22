4869acf48ce8576ab173a3a152301a866911a56c
Vlad Poskatcheev <vposkatcheev@gmail.com>
2016-01-22 13:25:38 -0800

    Time in Words Challenge - finished and working

--- src/main/java/hackrank/algorithm/implement/time/Solution.java
+++ src/main/java/hackrank/algorithm/implement/time/Solution.java
@@ -10,29 +10,68 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main( String[] args ) {
-		
+
 		Scanner scanner = new Scanner( System.in );
 		int hours = scanner.nextInt();
 		int minutes = scanner.nextInt();
 		scanner.close();
 
-		System.out.println( toWords( hours, minutes ) );
-	}
-	
-	public static String toWords( int hours, int minutes ) {
-		
-		/*
-		5:00 five o' clock
-		5:01 one minute past five
-		5:10 ten minutes past five
-		5:30 half past five
-		5:40 twenty minutes to six
-		5:45 quarter to six
-		5:47 thirteen minutes to six
-		5:28 twenty eight minutes past five
-		*/
-		
-		return "Write me";
+		System.out.println( TimeWordFormatter.getInstance().format( hours, minutes ) );
 	}
 
+}
+
+class TimeWordFormatter {
+
+	private final static TimeWordFormatter formatter = new TimeWordFormatter();
+
+	public static TimeWordFormatter getInstance() {
+		return formatter;
+	}
+
+	private String[] words;
+
+	private TimeWordFormatter() {
+		words = new String[] { 
+				"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+				"twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty" 
+		};
+	}
+
+	private String getNumberWord( int number ) {
+		if ( number <= 20 ) {
+			return words[ number ];
+		} else {
+			return words[ 20 ] + " " + words[ number - 20 ];
+		}
+	}
+
+	private String getMinutesWord( int minutes ) {
+		if ( minutes > 30 ) {
+			minutes = 60 - minutes;
+		}
+
+		return getNumberWord( minutes ) + " minute" + (minutes == 1 ? "" : "s");
+	}
+
+	public String format( int hours, int minutes ) {
+
+		String hourWord = getNumberWord( (minutes > 31 ? (hours + 1) : hours) );
+
+		if ( minutes == 0 ) {
+			return hourWord + " o' clock";
+		} else if ( minutes == 15 ) {
+			return "quarter past " + hourWord;
+		} else if ( minutes < 30 ) {
+			return getMinutesWord( minutes ) + " past " + hourWord;
+		} else if ( minutes == 30 ) {
+			return "half past " + hourWord;
+		} else if ( minutes == 45 ) {
+			return "quarter to " + hourWord;
+		} else if ( minutes <= 59 ) {
+			return getMinutesWord( minutes ) + " to " + hourWord;
+		} else {
+			return "";
+		}
+	}
 }
